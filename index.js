120b4262e316f4838e377474455b3921d7d672d1
EthanZhong <zhongdings@163.com>
2016-06-16 22:23:55 +0800

    addfiles

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ var Client=require('./lib/client');
 
 Room.maxRooms=500;
 Room.maxClients=4;
+Room.timeOut=1000;
 
-Room.createRoom('ethan');
+var room=Room.createRoom('ethan');
+room.addClient('ethan')
+room.addClient('2')
+room.addClient('3')
+
+//_.range(4);
+//_.padStart(55555,4,'0');
+//_.times(5,_.constant(false));
